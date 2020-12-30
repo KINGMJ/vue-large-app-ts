dfbc86f3a4d243ccc470ce7e2293372f9edc0278
KINGMJ <328047478@qq.com>
2020-12-30 22:06:39 +0800

    eslint和prettierrc配置

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,11 +5,7 @@ module.exports = {
     node: true
   },
 
-  extends: [
-    'plugin:vue/vue3-essential',
-    '@vue/standard',
-    '@vue/typescript/recommended'
-  ],
+  extends: ['plugin:vue/vue3-essential', '@vue/standard', '@vue/typescript/recommended'],
 
   parserOptions: {
     ecmaVersion: 2020
@@ -17,24 +13,19 @@ module.exports = {
 
   rules: {
     'no-console': 'off',
-    'no-debugger': 'off'
+    'no-debugger': 'off',
+    'space-before-function-paren': 0
   },
 
   overrides: [
     {
-      files: [
-        '**/__tests__/*.{j,t}s?(x)',
-        '**/tests/unit/**/*.spec.{j,t}s?(x)'
-      ],
+      files: ['**/__tests__/*.{j,t}s?(x)', '**/tests/unit/**/*.spec.{j,t}s?(x)'],
       env: {
         jest: true
       }
     }
   ],
 
-  'extends': [
-    'plugin:vue/essential',
-    '@vue/standard',
-    '@vue/typescript/recommended'
-  ]
+  extends: ['plugin:vue/essential', '@vue/standard', '@vue/typescript/recommended'],
+  extends: ['plugin:vue/essential', '@vue/standard', '@vue/typescript/recommended']
 }
