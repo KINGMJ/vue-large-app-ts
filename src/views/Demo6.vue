239ae138e850cb795243efa66a4176ccf7c45104
KINGMJ <328047478@qq.com>
2021-01-03 12:57:03 +0800

    函数

---
+++ src/views/Demo6.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>函数</div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import Demo6Service from '@/services/demo6.ts'
+
+export default defineComponent({
+  name: 'Demo6',
+  setup() {
+    Demo6Service.bootstrap()
+  }
+})
+</script>
