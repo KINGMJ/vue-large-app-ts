c6e94be7a5f315cb61a2796e8a8b2f8ebc78c0f6
KINGMJ <328047478@qq.com>
2021-01-04 18:13:31 +0800

    Using with Options API

---
+++ src/components/demo1/Component1.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <p>{{ name }}</p>
+    <span>{{ book.title }}</span>
+  </div>
+</template>
+
+<script lang="ts">
+import { Book } from '@/types'
+import { defineComponent, PropType } from 'vue'
+
+export default defineComponent({
+  name: 'Component1',
+  props: {
+    name: String,
+    success: { type: String },
+    callback: {
+      type: Function as PropType<() => void>
+    },
+    book: {
+      type: Object as PropType<Book>,
+      require: true
+    }
+  },
+  mounted() {
+    this.callback()
+  }
+})
+</script>
--- src/views/Demo1.vue
+++ src/views/Demo1.vue
@@ -1,22 +1,46 @@
 <template>
   <div>Using with Options API</div>
+  <br />
+  <Component1 name="这是一个测试组件" :book="book" :callback="log()" />
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
-
-interface Book {
-  title: string
-  author: string
-  year: number
-}
+import { Book } from '@/types'
+import Component1 from '@/components/demo1/Component1.vue'
 
 export default defineComponent({
   name: 'Demo1',
   data() {
     return {
-      book {}
+      message: 'Hello',
+      book: {
+        title: 'Vue 3 Guide',
+        author: 'Vue Team',
+        year: 2020
+      } as Book
     }
+  },
+  computed: {
+    greeting(): string {
+      return this.message + '!'
+    },
+    greetingUppercased: {
+      get(): string {
+        return this.greeting.toUpperCase()
+      },
+      set(newValue: string) {
+        this.message = newValue.toUpperCase()
+      }
+    }
+  },
+  methods: {
+    log() {
+      console.log('测试输出')
+    }
+  },
+  components: {
+    Component1
   }
 })
 </script>
