fc812a36f86c55c9ca0748d3b836077fb5ec8a91
KINGMJ <328047478@qq.com>
2021-01-06 17:47:18 +0800

    Using with Composition API

--- src/components/demo1/Component2.vue
+++ src/components/demo1/Component2.vue
@@ -4,7 +4,7 @@
 </template>
 
 <script lang="ts">
-import { defineComponent, reactive, ref } from 'vue'
+import { computed, defineComponent, reactive, ref } from 'vue'
 import { Book } from '@/types'
 
 export default defineComponent({
@@ -14,6 +14,12 @@ export default defineComponent({
     // const book = reactive<Book>({ title: 'ES6指南' })
     // const book = reactive({ title: 'ES6指南' })
     const book = reactive({ title: 'ES6指南' }) as Book
+
+    const count = ref(1)
+    const doubleCount = computed(() => count.value * 2)
+    // error
+    // const result = doubleCount.value.split('')
+    console.log(doubleCount)
     return {
       year,
       book
