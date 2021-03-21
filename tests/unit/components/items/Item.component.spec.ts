c23b932d10fa62029da17b14b78f8ca19c0e5a8b
KINGMJ <328047478@qq.com>
2021-03-21 18:59:39 +0800

    chapter5: 完善item组件测试

--- tests/unit/components/items/Item.component.spec.ts
+++ tests/unit/components/items/Item.component.spec.ts
@@ -13,6 +13,40 @@ describe('Item.component.vue', () => {
         model: model
       }
     })
-    expect(wrapper.text()).toMatch('Unit test item 1')
+    // expect(wrapper.text()).toMatch('Unit test item 1')
+    const domEl = wrapper.find('div.name')
+    expect(domEl.text()).toStrictEqual('Unit test item 1')
+  })
+
+  it('has expected css class when selected is false', () => {
+    const model: ItemInterface = {
+      id: 2,
+      name: 'Unit test item 2',
+      selected: false
+    }
+    const wrapper = shallowMount(ItemComponent, {
+      props: {
+        model: model
+      }
+    })
+    const classes = wrapper.classes()
+    expect(classes).toContain('item')
+    expect(classes).not.toContain('selected')
+  })
+
+  it('has expected css class when selected is true', () => {
+    const model: ItemInterface = {
+      id: 3,
+      name: 'Unit test item 3',
+      selected: true
+    }
+    const wrapper = shallowMount(ItemComponent, {
+      props: {
+        model: model
+      }
+    })
+    const classes = wrapper.classes()
+    expect(classes).toContain('item')
+    expect(classes).toContain('selected')
   })
 })
