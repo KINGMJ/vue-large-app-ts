655191d37bf7b7d7c3383a5bf5afd07a702c9df2
KINGMJ <328047478@qq.com>
2021-03-21 17:33:24 +0800

    chapter3: 使用接口替换any

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -6,24 +6,28 @@
 
 <script lang="ts">
 import ItemsListComponent from '@/components/items/ItemsList.component.vue'
+import { ItemInterface } from '@/models/items/Item.interface'
 import { defineComponent } from 'vue'
 
 export default defineComponent({
   name: 'Home',
   components: { ItemsListComponent },
   setup() {
-    const items: any[] = [
+    const items: ItemInterface[] = [
       {
         id: 1,
-        name: 'Item 1'
+        name: 'Item 1',
+        selected: false
       },
       {
         id: 2,
-        name: 'Item 2'
+        name: 'Item 2',
+        selected: false
       },
       {
         id: 3,
-        name: 'Item 3'
+        name: 'Item 3',
+        selected: false
       }
     ]
 
