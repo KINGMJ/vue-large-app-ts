429ac84f1b085b8f9f71cb90b3fa7877cd1075fc
KINGMJ <328047478@qq.com>
2021-08-19 11:21:23 +0800

    新增两个切换翻译的组件

---
+++ src/components/locale-selector/LocaleFlagRadio.component.vue
@@ -0,0 +1,36 @@
+<template>
+  <label role="radio" :class="`locale-radio ${localeInfo.selected ? 'selected' : ''}`.trim()">
+    <i :class="`flag-icons ${localeInfo.flag}`"></i>
+    <input
+      type="radio"
+      class="icon-button"
+      name="locale"
+      :value="localeInfo.selected"
+      v-model="localeInfo.selected"
+      @click="onClick"
+    />
+  </label>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import { useI18n } from 'vue-i18n'
+import { LocaleInfoInterface } from '@/models/localization/LocalInfo.interface'
+import { i18n } from '@/plugins/vue-i18n-next-plugin'
+export default defineComponent({
+  props: {
+    localeInfo: {
+      type: Object
+    }
+  },
+  setup(props, { emit }) {
+    const i18n = useI18n()
+    const onClick = () => {
+      emit('clicked', props.localeInfo)
+    }
+    return {
+      onClick
+    }
+  }
+})
+</script>
